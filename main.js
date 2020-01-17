cc96682bad5b090e7ddd37f76e1252f6e8bf7679
Tomi Salo <tomi.a.salo@outlook.com>
2020-01-17 18:37:43 +0200

    Eka versio

---
+++ main.js
@@ -0,0 +1,10 @@
+function openSlideMenu(){
+    document.getElementById('side-menu').style.width = '250px';
+    
+  }
+
+  function closeSlideMenu(){
+    document.getElementById('side-menu').style.width = '0';
+   
+  }
+
